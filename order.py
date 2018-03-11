514f37425cb7d3003b9680be3cdb7dcf0f6d7cd9
Yan Sun <34657711+ysun94@users.noreply.github.com>
2018-03-11 23:09:56 +0000

    Update order.py
    Add match engine

--- order.py
+++ order.py
@@ -35,12 +35,12 @@ class LimitOrder(Order):
         self.price = price
 
     def signed_price(self):
-        if self.direction is Direction.buy:
+        if self.direction == Direction.buy.value:
             return +self.price
         else:
             return -self.price
 
-    def __gt__(self, other):
+    def __lt__(self, other):
         return (self.signed_price() > other.signed_price()) \
             or (self.signed_price() == other.signed_price()
                    and self.timestamp < other.timestamp)
@@ -49,17 +49,16 @@ class LimitOrder(Order):
         return (self.price == other.price) \
             and (self.timestamp == other.timestamp)
 
-    def __lt__(self, other):
-        return not (self.__gt__(other) or self.__eq__(other))
+    def __gt__(self, other):
+        return not (self.__lt__(other) or self.__eq__(other))
 
     def __str__(self):
-        return "The order %s information is shown as follows:\n" \
-               "----------------\n" \
-               "order_id: %s\nquantity: %d\n" \
-               "broker: %s\ntimestamp: %d\n" \
-               "direction: %d\nprice: %8.2f\n" \
-               "----------------" % \
-               (self.order_id, self.order_id, self.quantity, self.broker, self.timestamp, self.direction, self.price)
+        return "(" \
+               "order_id: %s, quantity: %d, " \
+               "broker: %s, timestamp: %d, " \
+               "direction: %d, price: %8.2f" \
+               ")" % \
+               (self.order_id, self.quantity, self.broker, self.timestamp, self.direction, self.price)
 
 
     def set_quantity(self, value):
@@ -78,35 +77,53 @@ class OrderBook(object):
         # insertion time complexity
 
     def add(self, lo):
-        if lo.direction is Direction.buy:
+        if lo.direction == Direction.buy.value:
+            print("A new bid is added. "
+                  "(" \
+               "order_id: %s, quantity: %d, " \
+               "broker: %s, price: %8.2f" \
+               ")" % \
+                (lo.order_id, lo.quantity, lo.broker, lo.price)
+)
             heapq.heappush(self.bids, lo)
             self.order_index[ lo.order_id ] = lo
         else:
             heapq.heappush(self.asks, lo)
+            print("A new ask is added. "
+                  "(" \
+               "order_id: %s, quantity: %d, " \
+               "broker: %s, price: %8.2f" \
+               ")" % \
+                (lo.order_id, lo.quantity, lo.broker, lo.price))
             self.order_index[ lo.order_id ] = lo
+        if self.asks and self.bids:
+            self.__match()
 
+    def __match(self):
+        best_bid = self.bids[0]
+        best_ask = self.asks[0]
+        if best_bid.price >= best_ask.price:
+            if best_bid.quantity > best_ask.quantity:
+                remain = best_bid.quantity - best_ask.quantity
+                print('A bid (ID: %s) and an ask (ID: %s) are matched. '
+                      'The ask (ID: %s) is filled with the quantity %d.' \
+                      % (best_bid.order_id, best_ask.order_id, best_ask.order_id, best_ask.quantity))
+                self.bids[0].set_quantity(remain)
+                heapq.heappop(self.asks)
+                self.__match()
 
-'''
-order1019 = LimitOrder('1019', 62, 'L', 1, 1, 10702)
-order1006 = LimitOrder('1006', 8, 'R',1, 1, 10665)
-order1004 = LimitOrder('1004', 74, 'C', 2, 0, 9092)
-order1012 = LimitOrder('1012', 92, 'H', 2, 1, 9684)
-order1005 = LimitOrder('1005', 83, 'X', 4, 1, 9841)
-order1017 = LimitOrder('1017', 89, 'D', 5, 1, 9784)
-order1001 = LimitOrder('1001', 40, 'R', 5, 0, 9521)
-order1007 = LimitOrder('1007', 19, 'N', 5, 1, 10388)
-order1013 = LimitOrder('1013', 97, 'J', 6, 0, 9147)
-order1010 = LimitOrder('1010', 41, 'R', 7, 0, 10572)
-order1015 = LimitOrder('1015', 94, 'G', 8, 0, 10077)
-order1003 = LimitOrder('1003', 91, 'Q', 8, 1, 8695)
-order1009 = LimitOrder('1009', 59, 'S', 10, 0, 11066)
-order1018 = LimitOrder('1018', 68, 'F', 12, 0, 8225)
-order1008 = LimitOrder('1008', 3, 'K', 12, 1, 9849)
-order1016 = LimitOrder('1016', 4, 'D', 13, 0, 8726)
-order1002 = LimitOrder('1002', 83, 'O', 16, 0, 10876)
-order1011 = LimitOrder('1011', 38, 'D', 18, 0, 11142)
-order1014 = LimitOrder('1014', 54, 'Q', 19, 1, 9442)
-order1000 = LimitOrder('1000', 68, 'S', 20, 1, 9287)
-order1015 = LimitOrder('1015', 94, 'G', 8, 0, 10077)
-print(bids[-1]) do not use this
-'''
+            elif best_bid.quantity < best_ask.quantity:
+                remain = best_ask.quantity - best_bid.quantity
+                print('A bid (ID: %s) and an ask (ID: %s) are matched. '
+                      'The bid (ID: %s) is filled with the quantity %d.' \
+                      % (best_bid.order_id, best_ask.order_id, best_bid.order_id, best_bid.quantity))
+                self.asks[0].set_quantity(remain)
+                heapq.heappop(self.bids)
+                self.__match()
+
+            else:
+                print('A bid (ID: %s) and an ask (ID: %s) are matched. '
+                      'Both are filled with the quantity %d.' \
+                      % (best_bid.order_id, best_ask.order_id, best_ask.quantity))
+                heapq.heappop(self.bids)
+                heapq.heappop(self.asks)
